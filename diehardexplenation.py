e2a70b7c0d67271a2198d8e2313ba9654a4c21a0
an0nomuse <r1v475d3m153@gmail.com>
2024-08-21 13:55:29 -0700

    Add files via upload
    If I need to add anything let me know, but I believe this is all you need, specifically the .py file, to run the program. If you open it with a python ide
     like pycharm it should work.

---
+++ diehardexplenation.py
@@ -0,0 +1,16 @@
+i="die hard is a christmas movie"
+def die_hard():
+    return(i)
+
+answer=input("wanna know what die hard is?")
+if answer == "yes":
+    print(die_hard())
+elif answer == "no":
+    print("The result of months of learning and work, and you won't even let it run."
+          "Like, seriously, wtf dude, I'm trying to bring knowledge"
+          " and you swatted it away like a fly."
+          "That's some mad disrespect, my dude.")
+else:
+    print("enter yes or no")
+
+input("press enter to close this garbage")
